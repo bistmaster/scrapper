cac0e50087c122492e0d7f407420b07e6caea0ca
Bethoveen Todino <prime.bethoveen@gmail.com>
2014-07-17 13:23:25 +0800

    Testing Http connection

--- src/main/java/org/mit/impl/MITServiceImpl.java
+++ src/main/java/org/mit/impl/MITServiceImpl.java
@@ -1,5 +1,12 @@
 package org.mit.impl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.mit.bean.CourseDetail;
 import org.mit.bean.CourseList;
 import org.mit.service.MITService;
@@ -9,10 +16,38 @@ import org.mit.service.MITService;
  *
  */
 public class MITServiceImpl implements MITService{
-
-	public CourseList getCourseList(int provider) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private static String API = "http://data.oeconsortium.org/api/";
+	private static String API_VERSION = "v1";
+	private static String API_URL = API + API_VERSION + "/";
+	private int provider = 0;
+	
+	public MITServiceImpl(int _provider) {
+		this.provider = _provider;
+	}
+	
+	public void getConnection() {
+		try {
+			URL url = new URL(API_URL + "providers/" + provider + "/courses");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			
+			if(conn.getResponseCode() != 200){
+				throw new RuntimeException("Failed, HTTP error code ");
+			}
+			
+			InputStreamReader inputStream = new InputStreamReader(conn.getInputStream());
+			BufferedReader br = new BufferedReader(inputStream);
+			String output = "";
+			while((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+		} catch(MalformedURLException e){
+			
+		} catch(IOException e){
+			
+		}
 	}
 
 	public CourseDetail getCourseDetail(String linkHash) {
@@ -25,4 +60,15 @@ public class MITServiceImpl implements MITService{
 		
 	}
 
+	public CourseList getCourseList(int provider) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		MITServiceImpl service = new MITServiceImpl(13);
+		System.out.println("Fetching... ");
+		service.getConnection();
+	}
+		
 }
